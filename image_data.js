19c45206600997b674161527be366bbe94a7772e
phildonley <phillipdonleyterex@gmail.com>
2024-12-03 12:46:36 -0800

    Create image_data.js

---
+++ image_data.js
@@ -0,0 +1,77 @@
+const imageData = [
+  {
+    "ID": 10170163,
+    "displayName": "BUSHING,STRAIN RELIEF,.875/.40",
+    "description": "BUSHING,STRAIN RELIEF,.875/.40",
+    "longDescription": NaN,
+    "productNumber": "128118GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10170163.128118GT.JPG"
+    ]
+  },
+  {
+    "ID": 10203155,
+    "displayName": "TERMINAL,3/8 RING,12-10 GAUGE",
+    "description": "TERMINAL,3/8 RING,12-10 GAUGE",
+    "longDescription": NaN,
+    "productNumber": "45459GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10203155.45459GT-PIC103.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10203155.45459GT-PIC105.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10203155.45459GT-PIC107.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10203155.45459GT-PIC109.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10203155.45459GT-PIC111.jpeg"
+    ]
+  },
+  {
+    "ID": 10238,
+    "displayName": "PIN, COTTER, .125 X .75",
+    "description": "PIN, COTTER, .125 X .75",
+    "longDescription": "P/N 12016 IS A DIRECT REPLACEMENT TO P/N     6-6244-06.",
+    "productNumber": "12016GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10238.12016GT.JPG"
+    ]
+  },
+  {
+    "ID": 10265149,
+    "displayName": "ENCLOSURE,GC27-25/GC21-19,FRNT",
+    "description": "ENCLOSURE,GC27-25/GC21-19,FRNT",
+    "longDescription": NaN,
+    "productNumber": "T106366GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265149.T106366GT-PIC103.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265149.T106366GT-PIC105.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265149.T106366GT-PIC107.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265149.T106366GT-PIC109.jpeg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265149.T106366GT-PIC111.jpeg"
+    ]
+  },
+  {
+    "ID": 10265151,
+    "displayName": "ENCLOSURE,GC14-1,TOP",
+    "description": "ENCLOSURE,GC14-1,TOP",
+    "longDescription": "ALSO PURCHASE THE BOTTOM PLATE, T106370GT, WHEN ORDERING THIS ENCLOSURE",
+    "productNumber": "T106368GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265151.T106368GT-PIC103.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265151.T106368GT-PIC105.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265151.T106368GT-PIC107.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265151.T106368GT-PIC109.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265151.T106368GT-PIC111.jpg"
+    ]
+  },
+  {
+    "ID": 10265153,
+    "displayName": "ENCLOSURE,GC14-1/GC16-1,BOT",
+    "description": "ENCLOSURE,GC14-1/GC16-1,BOT",
+    "longDescription": "BOTTOM PIECE FOR BOTH T106368GT AND 1256658GT",
+    "productNumber": "T106370GT",
+    "images": [
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265153.T106370GT.PIC101.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265153.T106370GT.PIC102.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265153.T106370GT.PIC103.jpg",
+      "https://parts.genielift.com/ccstore/v1/images/?source=%2Ffile%2Fv8685995964176266201%2Fproducts%2F10265153.T106370GT.PIC104.jpg"
+    ]
+  }
+];
